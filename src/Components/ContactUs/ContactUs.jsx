7e291fa5a01757d1d1a131b2d84bac53902f8407
unknown <elzur.nitay@gmail.com>
2023-05-30 20:34:03 +0300

    finished layout functionality

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,17 +1,32 @@
 import './ContactUs.css'
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form"
 function ContactUs() {
-    const {register, formState: {errors}, handleSubmit} = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
     function onSubmit(data) {
         console.log(data);
     }
     return (
         <div id="contactMain">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                
-                <input type="submit" />
-            </form>
-        </div>
+            <form id='contactForm' onSubmit={handleSubmit(onSubmit)}>
+                <div className="bigFormText">Contact Us</div>
+                <div className="normalFormText">{errors && Object.values(errors).length > 0 ? Object.values(errors)[0]?.message :`So we can keep you up to date!`}</div>
+                <div id="contactFormNames">
+                    <input placeholder='First Name' className='contactFormInput' type="text" {
+                        ...register('firstName', { required: 'First Name is required' })
+                    } />
+                    <input className='contactFormInput' placeholder='Last Name' type="text" {
+                        ...register('lastName', { required: 'Last Name is required' })
+                    } />
+                </div>
+                <input className='contactFormInput' type="email" placeholder='Your Email Address'{
+                    ...register('email', { required: 'Email is required', pattern: {
+                        value: /.@./,
+                        message: 'Email must contain text before and after a @'
+                    } })
+                } />
+                <input id='contactFormButton' type="submit" />
+            </form >
+        </div >
     )
 }
 export default ContactUs;
