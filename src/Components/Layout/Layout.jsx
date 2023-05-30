49cf6600fa234f28bf24d44a7c1c72eb216971a9
unknown <elzur.nitay@gmail.com>
2023-05-30 13:55:30 +0300

    basic css

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,8 +1,10 @@
 import './Layout.css'
+import { Outlet, Link } from 'react-router-dom';
 function Layout() {
     return (
-        <>
-        </>
+        <div id='layoutMain'>
+            
+        </div>
     )
 }
 export default Layout;
