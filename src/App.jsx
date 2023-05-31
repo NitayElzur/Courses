18614b3d5240d878ad3bf71448c796eb61ab2794
unknown <elzur.nitay@gmail.com>
2023-05-31 09:02:58 +0300

    route home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './Components/Layout/Layout'
+import HomePage from './Components/HomePage/HomePage'
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout/>}/>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage/>}/>
+        </Route>
       </Routes>
     </>
   )
