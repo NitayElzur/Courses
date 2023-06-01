8322a58cfbffb1072c399344f2239f42bbd2b615
unknown <elzur.nitay@gmail.com>
2023-06-01 09:58:05 +0300

    -carousel

--- src/Components/ProfCarousel/ProfCarousel.jsx
+++ src/Components/ProfCarousel/ProfCarousel.jsx
@@ -1,6 +1,5 @@
 import "./ProfCarousel.css";
 import React from "react";
-import Carousel from "better-react-carousel";
 import json from "../../json/review.json";
 import { useState, useEffect } from "react";
 import ProfCard from "./ProfCard";
@@ -15,28 +14,10 @@ function ProfCarousel() {
 
   return (
     <div id="carousel">
-      <div id="prof-header">
+      <div id="prof-header"> 
         <h1>Our Proffesors:</h1>
       </div>
       <div id="carousel-container">
-        <Carousel
-          cols={2}
-          rows={1}
-          gap={100}
-          loop
-          containerStyle={{
-            zIndex: 0,
-            width: '90%'
-          }}
-        >
-          {detail &&
-            detail.map((value, index) => (
-              <Carousel.Item key={index}>
-                <ProfCard result={value} />
-              </Carousel.Item>
-            ))}
-        </Carousel>
-        <ProductPage/>
       </div>
     </div>
   );
--- src/Components/RevCarousel/RevCarousel.jsx
+++ src/Components/RevCarousel/RevCarousel.jsx
@@ -1,7 +1,6 @@
 import './RevCarousel.css'
 import json from '../../json/review.json'
 import { useEffect, useState } from 'react';
-import Carousel from 'better-react-carousel';
 import RevCard from '../RevCard/RevCard';
 function RevCarousel() {
     const [data, setData] = useState([]);
@@ -15,21 +14,7 @@ function RevCarousel() {
     return (
         <>
             <div id='revCarouselBigText'>Student Reviews:</div>
-            <Carousel id='revCarousellMain' rows={1} cols={1} gap={'10px'} loop autoplay={5000} containerStyle={{
-                width: windowWidth > 768 ? '70vw' : '95vw',
-                height: '30vh',
-                zIndex: 0
-            }}>
-                {data &&
-                    data.map((value, index) => {
-                        return (
-                            <Carousel.Item key={index}>
-                                <RevCard value={value} />
-                            </Carousel.Item>
-                        )
-                    })
-                }
-            </Carousel>
+        
         </>
     )
 }
