5c888c1fa91c3aa5e94d482b645c55358530fa5f
unknown <elzur.nitay@gmail.com>
2023-06-01 14:32:54 +0300

    carousel fix thank god

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import './Carousel.css'
+function Carousel({ children, infiniteLoop }) {
+    const [length, setLength] = useState(children.length)
+    const [touchPosition, setTouchPosition] = useState(null)
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX
+        setTouchPosition(touchDown)
+    }
+    useEffect(() => {
+        setLength(children.length)
+        let time;
+        if (infiniteLoop) {
+             time = setInterval(() => {
+                setCurrentIndex(prev => prev < length - 1 ? prev + 1 : 0)
+            }, 5000)
+        }
+        return () => {
+            clearInterval(time)
+        }
+    }, [length, children])
+    function next() {
+        if (currentIndex < (length - 1)) {
+            setCurrentIndex(prevState => prevState + 1)
+        }
+    }
+    function prev() {
+        if (currentIndex > 0) {
+            setCurrentIndex(prevState => prevState - 1)
+        }
+    }
+    const handleTouchMove = (e) => {
+        const touchDown = touchPosition
+
+        if (touchDown === null) {
+            return
+        }
+
+        const currentTouch = e.touches[0].clientX
+        const diff = touchDown - currentTouch
+
+        if (diff > 5) {
+            next()
+        }
+
+        if (diff < -5) {
+            prev()
+        }
+
+        setTouchPosition(null)
+    }
+    return (
+        <div id='carouselContainer'>
+            <div className='carouselWrapper'>
+                {currentIndex > 0 && <button className="leftArrow" onClick={prev}>
+                    &lt;
+                </button>}
+                {currentIndex < (length - 1) && <button className="rightArrow" onClick={next}>
+                    &gt;
+                </button>}
+                <div className='carouselContentWrapper' onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
+                    <div className='carouselContent' style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+export default Carousel;
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,14 +1,42 @@
 import './HomePage.css'
-import RevCarousel from '../RevCarousel/RevCarousel'
 import ProfCarousel from '../ProfCarousel/ProfCarousel'
+import Carousel from '../Carousel/Carousel';
+import RevCard from '../RevCard/RevCard';
+import json from '../../json/review.json'
+import { useEffect, useState } from 'react';
+import ProfCard from '../ProfCarousel/ProfCard';
 function HomePage() {
+    const [reviews, setReviews] = useState([]);
+    const [proffesors, setProffesors] = useState([]);
+    useEffect(() => {
+        setReviews(json.graduates);
+        setProffesors(json.proffesors);
+    }, [json])
     return (
         <div id='homeMain'>
             <div id="homeBanner">
                 <span>Ignite Your Passion for Technology through Expert Training</span>
             </div>
-            <ProfCarousel />
-            <RevCarousel />
+            <div>Our Proffesors:</div>
+            <Carousel>
+                {proffesors &&
+                    proffesors.map((value, index) => {
+                        return (
+                            <ProfCard key={index} result={value} />
+                        )
+                    })
+                }
+            </Carousel>
+                <div>Student Reviews:</div>
+            <Carousel infiniteLoop={true}>
+                {reviews &&
+                    reviews.map((value, index) => {
+                        return (
+                            <RevCard key={index} value={value} />
+                        )
+                    })
+                }
+            </Carousel>
         </div>
     )
 }
