baca2a8994087f2eb07f08e866a125f66c1e3086
unknown <elzur.nitay@gmail.com>
2023-06-02 15:12:01 +0300

    functional calendar plus sizing fix

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,19 +1,33 @@
 import './Calendar.css'
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { useEffect, useState } from "react";
-function Calendar({ data, setData }) {
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from 'react-router';
+import { MainContext } from '../../Contexts/MainContext';
+function Calendar() {
+    const { data, setData } = useContext(MainContext);
+    const navigate = useNavigate();
     const [width, setWidth] = useState(window.innerWidth);
+    const colors = [
+        '#32a852',
+        '#540206',
+        '#3244a8',
+        '#a0a832',
+        '#a83290',
+        '#7132a8',
+        '#04701f',
+        '#32e1ed',
+        '#66ffa6'
+    ]
     useEffect(() => {
         window.addEventListener('resize', () => setWidth(window.innerWidth))
         const tempArr = [];
-        data.map(value => {
-            const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        data.product?.map((value, index) => {
             tempArr.push({
                 title: `${value.course} - ${value.proffesor}`,
                 start: value['start-date'],
                 end: value['end-date'],
-                color: `#${randomColor}`
+                color: colors[index]
             })
         })
         setEvents(tempArr)
@@ -23,7 +37,9 @@ function Calendar({ data, setData }) {
         <FullCalendar
             plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
-            eventClick={(e) => console.log(e)}
+            eventClick={(e) => {
+                navigate(`/ProductPage/${data.product?.filter(value => value.proffesor === e.event.title.split(' - ')[1] && value.course === e.event.title.split(' - ')[0])[0].id}`)
+            }}
             events={events}
             aspectRatio={1}
             contentHeight={width > 768 ? width > 1100 ? width > 1500 ? width / 2.5 : width / 2 : width / 1.5 : width}
