d34fc52af870044db3ebef34fcda4c5f2e3138a0
unknown <elzur.nitay@gmail.com>
2023-06-02 11:55:50 +0300

    calendar 1st try

---
+++ src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,20 @@
+import './Calendar.css'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { useState } from "react";
+function Calendar() {
+    const [events, setEvents] = useState([
+        { title: 'FullStack Course - yonatan ben ezra', start: '2023-06-01', end: '2023-08-03', color: 'green' },
+        { title: 'DevOps Course', start: '2023-07-02', end: '2023-08-01' },
+    ])
+    return (
+        <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            eventClick={(e) => console.log(e)}
+            events={...events}
+            aspectRatio={window.innerHeight * 2 / window.innerWidth}
+        />
+    )
+}
+export default Calendar;
