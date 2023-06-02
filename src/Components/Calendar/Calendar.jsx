4183d5389122d12b63c1ddb73f8e0f99861aefad
unknown <elzur.nitay@gmail.com>
2023-06-02 12:12:08 +0300

    sizing

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,8 +1,12 @@
 import './Calendar.css'
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function Calendar() {
+    const [width, setWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        window.addEventListener('resize', () => setWidth(window.innerWidth))
+    }, [])
     const [events, setEvents] = useState([
         { title: 'FullStack Course - yonatan ben ezra', start: '2023-06-01', end: '2023-08-03', color: 'green' },
         { title: 'DevOps Course', start: '2023-07-02', end: '2023-08-01' },
@@ -14,7 +18,7 @@ function Calendar() {
             eventClick={(e) => console.log(e)}
             events={events}
             aspectRatio={1}
-            contentHeight={window.innerWidth}
+            contentHeight={width > 768 ? width > 1100 ? width > 1500 ? width / 2.5 : width / 2 : width / 1.5 : width}
         />
     )
 }
